f729d9f79c1c359b5e42c3c3e171b6d73d0ddf8a
Vivien-Shah <hfsthane7787@gmail.com>
2025-05-12 11:40:50 +0530

    Add files via upload

---
+++ class3.py
@@ -0,0 +1,36 @@
+'''x=5
+print(type(x))
+y=2.5
+print(type(y))
+z="book" 
+print(type(z))
+a=True
+print(type(a))
+
+#2 typecasting
+v="Vivien" 
+#b=int(v)
+#print(type(b))
+# string cannot be coverted to an integer
+c=15.2
+d=str(c)
+print(type(d))
+# integer can be coverted to a string and a float
+# float can also be converted to a string and float'''
+a="understandable" 
+b=a.index("l")
+print(b)
+c=a[5:10]
+print(c)
+print(a[2:5])
+d="laptop" 
+print(d[3:])
+print(d[:3])
+print(d[-1])
+print(d[::-1])
+name="Codingal"
+print(name[::-1])
+# concatenation
+e="Hello " 
+f="World" 
+print(e+f)
