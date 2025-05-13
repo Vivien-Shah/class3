acf033c679e2ba17a9c6dbef2aa5ba3d5b773739
Vivien-Shah <hfsthane7787@gmail.com>
2025-05-13 19:38:29 +0530

    Add files via upload

---
+++ class3-project1.py
@@ -0,0 +1,4 @@
+# Printing a greetinng message and converting it to uppercase.
+# mesg= "congratulations"
+# mesg= input("Hello Ram, Please Enter the Greeting Message for your friend:")
+print(mesg.upper())
